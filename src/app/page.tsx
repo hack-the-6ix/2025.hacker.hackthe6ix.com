9e18845ed8c89959c6b86e8325cc87084ab4f553
Justin Xue <43502772+shoexue@users.noreply.github.com>
2025-05-05 21:41:31 -0400

    Revert "insert components"
    This reverts commit ad9973e98bc46378f36b2883d76945f7a357060f.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,6 @@
-import Card from "@/components/Card";
-import Text from "@/components/Text";
+
 export default function Home() {
   return (
-   <div>
-                <Card
-                  pixelSize={4}
-                  radius={4}
-                  borderWidth={1}
-                  padding={4}
-                  borderColor="shades-100"
-                  backgroundColor={'#74A600'}
-                >
-                  <Text
-                    textType={'label'}
-                    textColor="white"
-                    textWeight="bold"
-                    className="mx-2"
-                  >
-                    Hacker Dashboard
-                  </Text>
-                </Card>
-    </div>
+   <div>Hacker Dash!!</div>
   );
 }
